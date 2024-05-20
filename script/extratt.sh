475f46a0b54806d5bb80a5e32ab2a415578dfa0b
LeoWang311 <88264474+ZJLEOWANG3@users.noreply.github.com>
2024-05-20 12:36:48 -0400

    Update extratt.sh

--- script/extratt.sh
+++ script/extratt.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #SBATCH -J extract_NT_DB
 #SBATCH -o extract_NT_DB.log
-#SBATCH -p short -c 1 -N 1 --time 24:00:00
+#SBATCH -p short -c 4 -N 1 --time 24:00:00
 
 ls nt.*.tar.gz > list
 
-for i in $(cat list); do
-  tar -zxf $i
-done
+## for multiple core parallel
+cat list | parallel -j $SLURM_CPUS_PER_TASK tar -zxf {} 
+# tar the last file because there is one file that all files share, which needs to be updated
+tar -zxf $(tail -n1 list)
+
+## for 1 core
+# for i in $(cat list); do
+#   tar -zxf $i
+# done
