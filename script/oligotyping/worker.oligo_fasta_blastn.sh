21677d9988b51e2be172248b1ae0e8f95f6525fd
LeoWang311 <88264474+ZJLEOWANG3@users.noreply.github.com>
2024-06-07 00:10:41 -0400

    Update worker.oligo_fasta_blastn.sh

--- script/oligotyping/worker.oligo_fasta_blastn.sh
+++ script/oligotyping/worker.oligo_fasta_blastn.sh
@@ -2,14 +2,20 @@
 
 #SBATCH -N1 -c8 -pshort
 #SBATCH --time 24:00:00
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
 
 input_list=$1; shift;
 taxid=$1;shift;
 
+echo $taxid
+
 blastn_dir="blastn"
 blast_db="$HOME/scratch/DATABASE/BLAST/nt"
 python_env_prefix="$HOME/.local/env/python-3.10.10-venv-generic"
-blastx_prefix="$HOME/opt/ncbi/blast+-2.13.0"
+blastx_prefix="$HOME/opt/ncbi/ncbi-blast-2.15.0+"
+
+maxmatch=20
 
 for fasta_full in $(cat $input_list); do
 
@@ -29,14 +35,19 @@ for fasta_full in $(cat $input_list); do
 		"-out" "$blastn_dir/$fasta.fna.blastn"
 		"-outfmt" "6 qseqid sacc pident evalue bitscore"
 		"-db" "$blast_db"
-		"-max_target_seqs" "20"
-		"-perc_identity" "99"
+		"-max_target_seqs" "$maxmatch"
 		"-num_threads" "$SLURM_CPUS_PER_TASK")
 	
+#		"-perc_identity" "99"
+
 	if [ -n "$taxid" ]; then
 		blast_cmd+=("-taxids" "$taxid") 
 	fi
 	
+	echo "Running blastn with command: ${blast_cmd[@]}" 1>&2
+	# Run the blastn command
+	"${blast_cmd[@]}"
+
 	# blastdbcmd
 	cut -f2 -d '	' $blastn_dir/$fasta.fna.blastn > $blastn_dir/$fasta.fna.blastn.hit_accs
 	$blastx_prefix/bin/blastdbcmd \
